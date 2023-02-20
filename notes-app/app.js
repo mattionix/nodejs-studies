04f48635362ab9eabbfff329a4529e660b3114a9
Rafael Mattioni <rafael@mattioni.me>
2023-02-19 22:27:55 -0300

    Fixing Submodule Issue

---
+++ notes-app/app.js
@@ -0,0 +1,68 @@
+import chalk from 'chalk'
+import notes from './notes.js'
+import yargs from 'yargs'
+import { hideBin } from 'yargs/helpers'
+
+const userCommand = yargs(hideBin(process.argv))
+
+// Create add command.
+userCommand
+    .command({
+        command: 'add',
+        describe: 'Use this command to create a brand new note!',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            },
+            body: {
+                describe: 'Note content',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: function (argv) {
+            notes.addNote(argv.title, argv.body)
+        }
+    })
+
+// Create remove command.
+userCommand
+    .command({
+        command: 'remove',
+        describe: 'Use this command to remove a note, given its title.',
+        builder: {
+            title: {
+                describe: 'Note title',
+                demandOption: true,
+                type: 'string'
+            }
+        },
+        handler: function (argv) {
+            notes.removeNote(argv.title)
+        }
+    })
+
+// Create list notes command
+userCommand
+    .command({
+        command: 'list',
+        describe: 'List all notes',
+        handler: function () {
+            console.log('This is the handler to listing all notes')
+        }
+    })
+
+// Create read note command
+userCommand
+    .command({
+        command: 'read',
+        describe: 'Read a given note',
+        handler: function () {
+            console.log('This is the handler to reads one specific note')
+        }
+    })
+
+// Parse our set of commands
+userCommand.parse()
---
+++ notes-app/notes.js
@@ -0,0 +1,107 @@
+import chalk from 'chalk'
+import * as fs from 'fs'
+
+const getNotes = function () {
+    return "Your notes..."
+}
+/**
+ * Adds a new note to notes database.
+ * 
+ * @param {String} title 
+ * @param {String} body 
+ */
+const addNote = function (title, body) {
+    // Load previous notes.
+    const notes = loadNotes()
+
+    // Look up for duplicated notes to avoid same titles.
+    const duplicatedNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    // If there's any duplicated note.
+    if (duplicatedNotes.length === 0) {
+        // Add new note in the end of notes list.
+        notes.push({
+            title: title,
+            body: body
+        })
+
+        // Save note.
+        saveNotes(notes)
+
+        console.log(chalk.bgGreen.bold("Success!"), "note created!")
+    } else {
+        // There's duplicated note, throw error.
+       console.log(chalk.bgRed.bold("Fail..."), "there's anoter note with same title.") 
+    }
+
+}
+
+/**
+ * Stringify the notes data and save it to file system.
+ * 
+ * @param {Array} notes 
+ */
+const saveNotes = function (notes){
+    // Stringify notes received.
+    const dataJSON = JSON.stringify(notes)
+
+    // Writes on file system.
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+/**
+ * Loads all the notes in the database.
+ * 
+ * @returns Parsed JSON of notes
+ */
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+/**
+ * Given a note title, remove it from the database.
+ * 
+ * @param {String} title 
+ */
+const removeNote = function(title) {
+    // Load existing notes.
+    const notes = loadNotes();
+    
+    // Filter the loaded notes, removing if title found.
+    let removedNotesCounter = 0
+    const notesToSave = notes.filter(function (note) {
+        if (note.title === title) {
+            // If note found by title, increase removed counter
+            removedNotesCounter++
+            // return false to remove from notes list.
+            return false
+        }
+        return true
+    })
+
+    // If there's note removed.
+    if (removedNotesCounter > 0) {
+        // Log to user.
+        console.log(chalk.bgGreen.bold('Success!'), title + " note removed!")
+
+        // Save new notes array.
+        saveNotes(notesToSave)
+        return
+    }
+
+    console.log(chalk.bgRed.bold('Ooppss!'), "there's no note with called " + title + ":'(")
+}
+
+export default {
+    getNotes: getNotes,
+    addNote: addNote,
+    removeNote: removeNote
+}
